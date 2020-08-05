b2a93e9c075c7ef1dc5470530a267daddb98595d
hanzo-genji <chenjinjin233@outlook.com>
2020-08-05 17:09:15 +0800

    添加全局样式和全局变量

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,49 @@
 <template>
   <div>
-    <Layout>M</Layout>
+    <Layout>
+      <div class="tags">
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+      </div>
+      <div>
+        <label class="notes">
+          <span class="name"></span>
+          <input type="text" />
+        </label>
+      </div>
+      <div>
+        <ul class="types">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+      <div>
+        <div>100</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </div>
+    </Layout>
   </div>
 </template>
 
@@ -11,4 +54,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/style/helper.scss";
 </style>
