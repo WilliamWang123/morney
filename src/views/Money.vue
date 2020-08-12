1daba3bfa3c9e4cbb89e35987206e47bbc240803
hanzo-genji <chenjinjin233@outlook.com>
2020-08-12 13:23:24 +0800

    展示和创建标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,15 +14,16 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
-import model from "@/model.ts";
-
-const recordList = model.fetch();
+import recordListModel from "@/models/recordListModel.ts";
+import tagListModel from "@/models/tagListModel.ts";
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 @Component({
   components: { NumberPad, Types, Notes, Tags },
 })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行"];
+  tags = tagList;
   recordList = recordList;
   record = {
     tags: [],
@@ -37,13 +38,13 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2 = model.clone(this.record);
+    const record2 = recordListModel.clone(this.record);
     record2.createAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
   onRecordListChange() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 }
 </script>
