9f862536848ab385f0be2a36be5ff05c3f030f79
hanzo-genji <chenjinjin233@outlook.com>
2020-08-15 11:34:21 +0800

    封装 window.createTagg

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -27,13 +27,7 @@ export default class Labels extends Vue {
   createTag() {
     const name = window.prompt("请输入标签名");
     if (name) {
-      const message = tagListModel.create(name);
-      if (message === "duplicated") {
-        window.alert("标签名重复了");
-      }
-      if (message === "success") {
-        window.alert("添加成功");
-      }
+      (window as any).createTag(name);
     }
   }
 }
