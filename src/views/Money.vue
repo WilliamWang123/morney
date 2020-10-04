994cbab661b83dd5dccd71558d51ba5f1482fcf7
恋仔 <1842563969@qq.com>
2020-10-04 21:51:57 +0800

    添加 createAt 输入框

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,10 +2,22 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Tabs :data-source="recordTypeList" :value.sync="record.type" />
-    <div class="notes">
-      <FormItem field-name="备注" placeholder="在这里输入备注" :value.sync="record.notes" />
+    <div class="createAt">
+      <FormItem
+        field-name="日期"
+        type="date"
+        placeholder="在这里输入日期"
+        :value.sync="record.createAt"
+      />
     </div>
-    <Tags @update:value="record.tags=$event" />
+    <div class="notes">
+      <FormItem
+        field-name="备注"
+        placeholder="在这里输入备注"
+        :value.sync="record.notes"
+      />
+    </div>
+    <Tags @update:value="record.tags = $event" />
   </Layout>
 </template>
 
@@ -30,6 +42,7 @@ export default class Money extends Vue {
     notes: "",
     type: "-",
     amount: 0,
+    createAt: new Date().toISOString(),
   };
   created() {
     this.$store.commit("fetchRecords");
